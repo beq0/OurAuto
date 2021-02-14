a8315b109088e73c2d06e1649c3642dd826a4b5d
Beka Gugulashvili <bgugu17@freeuni.edu.ge>
2021-02-14 15:44:28 +0400

    not local

--- Back/public/Front/scripts/addresses.js
+++ Back/public/Front/scripts/addresses.js
@@ -1,4 +1,4 @@
-const IS_LOCAL = true; 
+const IS_LOCAL = false; 
 const SERVICE_URL = IS_LOCAL ? 'http://localhost:4201/api' : 'http://165.22.30.90:4201/api';
 const USER_SERVICE_URL = IS_LOCAL ? 'http://localhost:4201/api/user' : 'http://165.22.30.90:4201/api/user';
 const CAR_SERVICE_URL = IS_LOCAL ? 'http://localhost:4201/api/car' : 'http://165.22.30.90:4201/api/car';
