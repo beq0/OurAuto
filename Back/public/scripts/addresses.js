53793358fdc0d6a2492467c5bd21fee9d0a20226
Beka Gugulashvili <bgugu17@freeuni.edu.ge>
2021-02-14 21:35:16 +0400

    fixed public addresses

--- Back/public/scripts/addresses.js
+++ Back/public/scripts/addresses.js
@@ -1,5 +1,4 @@
-const IS_LOCAL = true; 
-const SERVICE_URL = IS_LOCAL ? 'http://localhost:4201/api' : 'http://165.22.30.90:4201/api';
-const USER_SERVICE_URL = IS_LOCAL ? 'http://localhost:4201/api/user' : 'http://165.22.30.90:4201/api/user';
-const CAR_SERVICE_URL = IS_LOCAL ? 'http://localhost:4201/api/car' : 'http://165.22.30.90:4201/api/car';
-const FIXED_DATA_SERVICE_URL = IS_LOCAL ? 'http://localhost:4201/api/fixedData' : 'http://165.22.30.90:4201/api/fixedData';
+const SERVICE_URL = 'http://165.22.30.90:4201/api';
+const USER_SERVICE_URL = 'http://165.22.30.90:4201/api/user';
+const CAR_SERVICE_URL = 'http://165.22.30.90:4201/api/car';
+const FIXED_DATA_SERVICE_URL = 'http://165.22.30.90:4201/api/fixedData';
